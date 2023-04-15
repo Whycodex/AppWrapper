d66e7af90ba5180dec1ccc950bb3e6a5aac73172
Whycodex <neerajgupta12300@gmail.com>
2023-04-16 02:09:24 +0530

    Added Proof Of Concept for Communication

--- App.js
+++ App.js
@@ -1,20 +1,96 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useRef, useState } from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Alert,
+  Platform
+} from 'react-native';
+import { WebView } from 'react-native-webview';
+import * as Notifications from 'expo-notifications';
 
-export default function App() {
+const App = () => {
+  const [message, setMessage] = useState("");
+  const [askPermission, setAskPermission] = useState(false);
+  const [expoPushToken, setExpoPushToken] = useState('');
+
+  async function configureNotification() {
+    const { status } = await Notifications.getPermissionsAsync();
+    let finalStatus = status;
+
+    if (finalStatus !== 'granted') {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+
+    if (finalStatus !== 'granted') {
+      Alert.alert('Permissions Required', 'Cannot proceed without appropriate permissions');
+      return;
+    }
+
+    const pushTokenData = await Notifications.getExpoPushTokenAsync();
+    console.log(pushTokenData.data);
+    setExpoPushToken(pushTokenData.data);
+
+    if (Platform.OS === 'android') {
+      Notifications.setNotificationChannelAsync('default', {
+        name: 'default',
+        importance: Notifications.AndroidImportance.DEFAULT
+      })
+    }
+  }
+
+  useEffect(() => {
+    if (askPermission) {
+      configureNotification();
+    }
+  }, [askPermission]);
+
+  function onMessage(data) {
+    // console.log(data);
+    setMessage(data.nativeEvent.data);
+    if (message == 'login') {
+      setAskPermission(true);
+    }
+  }
+
+  function sendDataToWebView() {
+    webviewRef.postMessage(expoPushToken);
+  }
+
+  const webviewRef = useRef();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{ flex: 1, backgroundColor: "yellow" }}>
+      <View style={{ alignItems: 'center' }}>
+        <TouchableOpacity
+          onPress={() => sendDataToWebView()}
+          style={{
+            padding: 20,
+            width: 300,
+            marginTop: 100,
+            backgroundColor: 'black',
+            alignItems: 'center',
+          }}>
+          <Text style={{ fontSize: 20, color: 'white' }}>
+            Logout
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <WebView
+        ref={webviewRef}
+        scalesPageToFit={false}
+        mixedContentMode="compatibility"
+        style={{ backgroundColor: "red" }}
+        onMessage={onMessage}
+        source={{ uri: "https://charming-muffin-2d3b8e.netlify.app/" }}
+      />
+      <Text>Text Recieved from Webview: {message}</Text>
+      <Text>Token: {expoPushToken}</Text>
+    </SafeAreaView>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
